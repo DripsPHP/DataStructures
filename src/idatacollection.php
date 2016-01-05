0c5a5cacd28617dc33100dcad199a4762dc8a142
Raffael Kessler <raffael.kessler@prowect.com>
2016-01-05 16:57:07 +0100

    IDataCollection hinzugefügt

--- src/idatacollection.php
+++ src/idatacollection.php
@@ -4,5 +4,11 @@ namespace Drips\DataStructures;
 
 interface IDataCollection
 {
-    
+    public function get($key);
+
+    public function set($key, $value);
+
+    public function has($key);
+
+    public function getAll();
 }
